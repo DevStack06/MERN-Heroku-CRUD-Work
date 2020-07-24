42f33cc874f223ab4eee8d19144bdea0e70d96cb
Balram Rathore <balram0698@gmail.com>
2020-07-24 23:54:47 +0530

    almost complepeted

--- frontend/src/Container/HomeBuilder.js
+++ frontend/src/Container/HomeBuilder.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
-
+import "./Overall.css";
 class AppBuilder extends Component {
   render() {
     return (
-      <React.Fragment>
-        <h1>Welcome to home </h1>
-      </React.Fragment>
+      <div className="home">
+        <h1>Hi this is Balram Rathore. Welcome on SwitchOn Assignment</h1>
+        <h3 className="note">
+          You need to sign In or Sign Up first to use this Web App
+        </h3>
+        <div className="info">
+          For the testing you can login with the below credential-:
+        </div>
+        <div className="info">Username: Dev Stack</div>
+        <div className="info">Password: 12345</div>
+      </div>
     );
   }
 }
